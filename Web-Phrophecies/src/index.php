08d8bc9009a1e6550b25c9f2029a9f69bfc0c1a4
nellen <sebastian@nellen.it>
2014-10-13 22:33:13 +0200

    fixed raph's layout nightmare

--- Web-Phrophecies/src/index.php
+++ Web-Phrophecies/src/index.php
@@ -1,8 +1,3 @@
-
-<?php 
-echo "hello"; 
-?>
-
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
 "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 
@@ -11,8 +6,8 @@ echo "hello";
 <head>
 <title>The BreakFast Company</title>
 <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-<link rel="stylesheet" href="./css/layout.css" type="text/css"/>
-<link rel="stylesheet" href="./css/format.css" type="text/css"/>
+<link rel="stylesheet" href="./css/layout.css" type="text/css" />
+<link rel="stylesheet" href="./css/format.css" type="text/css" />
 </head>
 
 <body>
@@ -23,53 +18,53 @@ echo "hello";
 		<h3>Start your day with a smile!</h3> -->
 		</div>
 		<div id="breadcrumps">
-			<p><a href="./index.php">Home</a></p>
+			<p>
+				<a href="./index.php">Home</a>
+			</p>
 		</div>
-
-		<div id="left-area">
-			<ul class="sidebarlist">
-				<li><a href="./index.php">Home</a></li>
-				<li>Shop
-					<ul class="sidebarlist">
-						<li>Food</li>
-						<li>Beverage</li>
-						<li>Menus</li>
-						<li>Specials</li>
-					</ul>
-				</li>
-				<li><a href="./html/aboutus.html">About us</a></li>
-				<li><a href="./html/contact.html">Contact</a></li>
-			</ul>
-		</div>
-
-		<div id="right-area">
-			<div id="user-area">
+		<div id="main-area">
+			<div id="left-area">
 				<ul class="sidebarlist">
-					<li>Login / User menu
+					<li><a href="./index.php">Home</a></li>
+					<li>Shop
 						<ul class="sidebarlist">
-							<li><a href="./html/basket.html">Shopping Basket</a></li>
-							<li>User Profile
-								<ul class="sidebarlist">
-									<li>...</li>
-								</ul>
-							</li>
-							<li>Check out</li>
-							<li>Administration</li>
+							<li>Food</li>
+							<li>Beverage</li>
+							<li>Menus</li>
+							<li>Specials</li>
 						</ul>
 					</li>
+					<li><a href="./html/aboutus.html">About us</a></li>
+					<li><a href="./html/contact.html">Contact</a></li>
 				</ul>
 			</div>
-			<hr />
-			<div id="news-area">
-				<p>The news will be displayed here.</p>
+			<div id="content-area">
+				<h1>Webshop</h1>
+				<p>The content will be displayed in the content area.</p>
+			</div>
+			<div id="right-area">
+				<div id="user-area">
+					<ul class="sidebarlist">
+						<li>Login / User menu
+							<ul class="sidebarlist">
+								<li><a href="./html/basket.html">Shopping Basket</a></li>
+								<li>User Profile
+									<ul class="sidebarlist">
+										<li>...</li>
+									</ul>
+								</li>
+								<li>Check out</li>
+								<li>Administration</li>
+							</ul>
+						</li>
+					</ul>
+				</div>
+				<hr />
+				<div id="news-area">
+					<p>The news will be displayed here.</p>
+				</div>
 			</div>
 		</div>
-		
-		<div id="content-area">
-			<h1>Webshop</h1>
-			<p>The content will be displayed in the content area.</p>
-		</div>
-
 		<div id="footer-area">
 			<p>&copy; 2014 The BreakFast Company. All rights reserved.</p>
 		</div>
