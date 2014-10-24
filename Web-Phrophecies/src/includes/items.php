50a892885d34a59f6993c9c43d7ce611187c3a49
burkt4 <burkt4@bfh.ch>
2014-10-24 10:24:45 +0200

    category mit dynamischer Sprache

--- Web-Phrophecies/src/includes/items.php
+++ Web-Phrophecies/src/includes/items.php
@@ -1,30 +1,127 @@
 <?php
-$items = array(
+function getCategoryItems ($CatId, $lang) {
+	switch($lang) {
+		case "en":
+			$items = array(
 			array("name" => "Croissant", "image" => "./img/user-a.png", "price" => 1.50,
-				"discription" => "Homemade croissants. Freshly baked each morning.",
-				"variants" =>
-				array(
-					array("variantname" => "butter", "price" => 0.00),
-					array("variantname" => "whole-grain", "price" => 0.00)
-				)
+			"discription" => "Homemade croissants. Freshly baked each morning.",
+			"variants" =>
+			array(
+			array("variantname" => "butter", "price" => 0.00),
+			array("variantname" => "whole-grain", "price" => 0.00)
+			)
 			),
 			array("name" => "Bun", "image" => "./img/user-b.png", "price" => 1.50,
-				"discription" => "Homemade buns. Freshly baked each morning.",
-				"variants" =>
-				array(
-					array("variantname" => "butter", "price" => 0.00),
-					array("variantname" => "whole-grain", "price" => 0.00),
-					array("variantname" => "bacon", "price" => 0.50)
-				)
+			"discription" => "Homemade buns. Freshly baked each morning.",
+			"variants" =>
+			array(
+			array("variantname" => "butter", "price" => 0.00),
+			array("variantname" => "whole-grain", "price" => 0.00),
+			array("variantname" => "bacon", "price" => 0.50)
+			)
 			),
 			array("name" => "Bread", "image" => "./img/user-c.png", "price" => 2.00,
-				"discription" => "Homemade bread. Freshly baked each morning.",
-				"variants" =>
-				array(
-					array("variantname" => "butter", "price" => 0.00),
-					array("variantname" => "whole-grain", "price" => 0.00)
-				)
+			"discription" => "Homemade bread. Freshly baked each morning.",
+			"variants" =>
+			array(
+			array("variantname" => "butter", "price" => 0.00),
+			array("variantname" => "whole-grain", "price" => 0.00)
 			)
-		);
+			)
+			);
+			break;
+		case "de":
+			$items = array(
+			array("name" => "Gipfeli", "image" => "./img/user-a.png", "price" => 1.50,
+			"discription" => "Hausgemachte Gipfeli.  Jeden Morgen frisch gebacken.",
+			"variants" =>
+			array(
+			array("variantname" => "mit Butter", "price" => 0.00),
+			array("variantname" => "Vollkorn", "price" => 0.00)
+			)
+			),
+			array("name" => "Brötchen", "image" => "./img/user-b.png", "price" => 1.50,
+			"discription" => "Hausgemachte Brötchen. Jeden Morgen frisch gebacken.",
+			"variants" =>
+			array(
+			array("variantname" => "mit Butter", "price" => 0.00),
+			array("variantname" => "Vollkorn", "price" => 0.00),
+			array("variantname" => "Speck", "price" => 0.50)
+			)
+			),
+			array("name" => "Brot", "image" => "./img/user-c.png", "price" => 2.00,
+			"discription" => "Hausgemachtes Brot. Jeden Morgen frisch gebacken.",
+			"variants" =>
+			array(
+			array("variantname" => "mit Butter", "price" => 0.00),
+			array("variantname" => "Vollkorn", "price" => 0.00)
+			)
+			)
+			);
+			break;
+		case "fr":
+			$items = array(
+			array("name" => "Croissant", "image" => "./img/user-a.png", "price" => 1.50,
+			"discription" => "discription fr",
+			"variants" =>
+			array(
+			array("variantname" => "butter", "price" => 0.00),
+			array("variantname" => "whole-grain", "price" => 0.00)
+			)
+			),
+			array("name" => "Bun", "image" => "./img/user-b.png", "price" => 1.50,
+			"discription" => "discription fr",
+			"variants" =>
+			array(
+			array("variantname" => "butter", "price" => 0.00),
+			array("variantname" => "whole-grain", "price" => 0.00),
+			array("variantname" => "bacon", "price" => 0.50)
+			)
+			),
+			array("name" => "Bread", "image" => "./img/user-c.png", "price" => 2.00,
+			"discription" => "discription fr",
+			"variants" =>
+			array(
+			array("variantname" => "butter", "price" => 0.00),
+			array("variantname" => "whole-grain", "price" => 0.00)
+			)
+			)
+			);
+			break;
+		default:
+			$items = array(
+			array("name" => "Croissant", "image" => "./img/user-a.png", "price" => 1.50,
+			"discription" => "Homemade croissants. Freshly baked each morning.",
+			"variants" =>
+			array(
+			array("variantname" => "butter", "price" => 0.00),
+			array("variantname" => "whole-grain", "price" => 0.00)
+			)
+			),
+			array("name" => "Bun", "image" => "./img/user-b.png", "price" => 1.50,
+			"discription" => "Homemade buns. Freshly baked each morning.",
+			"variants" =>
+			array(
+			array("variantname" => "butter", "price" => 0.00),
+			array("variantname" => "whole-grain", "price" => 0.00),
+			array("variantname" => "bacon", "price" => 0.50)
+			)
+			),
+			array("name" => "Bread", "image" => "./img/user-c.png", "price" => 2.00,
+			"discription" => "Homemade bread. Freshly baked each morning.",
+			"variants" =>
+			array(
+			array("variantname" => "butter", "price" => 0.00),
+			array("variantname" => "whole-grain", "price" => 0.00)
+			)
+			)
+			);
+			break;
+	}
+	return $items; // a real function
+}
+
+
+
 
 ?>
--- Web-Phrophecies/src/sites/category.php
+++ Web-Phrophecies/src/sites/category.php
@@ -1,12 +1,16 @@
 <?php 
-					
+					$lang = "de";
+					$priceLabel = array("en"=>"Price","de"=>"Preis", "fr"=>"Prix");
+					$variationsLabel = array("en"=>"Variations","de"=>"Variationen", "fr"=>"Variations");
+					$basketButtonLabel = array("en"=>"Add to basket","de"=>"in den Warenkorb", "fr"=>"Ajouter au panier");
 					include_once("./includes/items.php");
+					$items = getCategoryItems(1, $lang);
 					foreach ($items as $item) {
 						echo "<div class=\"list-article\">";
 						echo "<img src=\"".$item["image"]."\" width=\"100px\" height=\"100px\"/>";
 						echo "<h2><a href=\"./croissant.html\">".$item["name"]."</a></h2>";
-						echo "<p class=\"list-article-price\">Price: ".$item["price"]." CHF</p>";
-						echo "<p class=\"list-article-variation\">Variations:";
+						echo "<p class=\"list-article-price\">$priceLabel[$lang]: ".$item["price"]." CHF</p>";
+						echo "<p class=\"list-article-variation\">$variationsLabel[$lang]:";
 						$firstElement = TRUE;
 						foreach ( $item["variants"] as $variant){
 							if ($firstElement) {
@@ -20,7 +24,7 @@
 
 						echo "</p>";
 						echo "<p>".$item["discription"]."</p>";
-						echo "<button onclick=\"msgAddtoBasket()\">Add to basket</button>";
+						echo "<button onclick=\"msgAddtoBasket()\">$basketButtonLabel[$lang]</button>";
 						echo "</div>";
 					}
 					?>
