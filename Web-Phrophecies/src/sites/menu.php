102b3e51ad3e7acc3f3274dd9137a2d08c5f71c4
nellen <sebastian@nellen.it>
2014-10-27 20:09:27 +0100

    cleanup

--- Web-Phrophecies/src/sites/menu.php
+++
@@ -1,4 +0,0 @@
-<?php
-	echo "Menu";
-	echo "<img src=\"./img/under_construction.png\" width=\"100%\" height=\"100%\" />";
-?>
--- Web-Phrophecies/src/sites/specials.php
+++
@@ -1,4 +0,0 @@
-<?php
-	echo "Specials";
-	echo "<img src=\"./img/under_construction.png\" width=\"100%\" height=\"100%\" />";
-?>
