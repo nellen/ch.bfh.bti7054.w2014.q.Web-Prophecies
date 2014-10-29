fa3c803ca8f12ca46359d521a4f620f3a2219418
burkt4 <burkt4@bfh.ch>
2014-10-29 01:27:35 +0100

    early draft task 6.3 and passing values for confirmation page

--- Web-Phrophecies/src/sites/billing.php
+++ Web-Phrophecies/src/sites/billing.php
@@ -1,8 +1,17 @@
 <?php 
 
-echo "Billing";
-echo $_POST["artId"];
-echo $_POST["varId"];
-echo "<img src=\"./img/under_construction.png\" width=\"100%\" height=\"100%\" />";
+$lang = get_language ();
+
+echo "<form action=\"index.php?site=confirm&lang=".$lang."\" method=\"post\">";
+echo "<input type=\"hidden\" name=\"artId\" value=\"". $_POST["artId"]. "\" /input>";
+echo "<input type=\"hidden\" name=\"varId\" value=\"". $_POST["varId"]. "\" /input>";
+echo "Firstname: <input type=\"text\" name=\"firstname\"  /input><br/>";
+echo "Lastname: <input type=\"text\" name=\"lastname\"  /input><br/>";
+echo "City: <input type=\"text\" name=\"city\"  /input><br/>";
+echo "Zip Code: <input type=\"text\" name=\"zip\"  /input><br/>";
+echo "Street: <input type=\"text\" name=\"street\"  /input><br/>";
+echo "E-mail: <input type=\"text\" name=\"email\"  /input><br/>";
+echo "<input class=\"billing-button\" type=\"submit\" value=\"Submit\"/>";
+echo "</form>";
 
 ?>
---
+++ Web-Phrophecies/src/sites/confirm.php
@@ -0,0 +1,13 @@
+<?php 
+
+echo "artId:". $_POST["artId"]. "<br/>";
+echo "varId:". $_POST["varId"]. "<br/>";
+echo "Firstname:". $_POST["firstname"]. "<br/>";
+echo "Lastname:". $_POST["lastname"]. "<br/>";
+echo "City:". $_POST["city"]. "<br/>";
+echo "Zip Code:". $_POST["zip"]. "<br/>";
+echo "Street:". $_POST["street"]. "<br/>";
+echo "E-mail:". $_POST["email"]. "<br/>";
+
+
+?>
