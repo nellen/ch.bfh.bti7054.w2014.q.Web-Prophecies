cf166d4913a78a81622d371be10c242103a68586
burkt4 <burkt4@bfh.ch>
2014-11-03 20:21:38 +0100

    added missing VarIds

--- Web-Phrophecies/src/includes/items.php
+++ Web-Phrophecies/src/includes/items.php
@@ -36,25 +36,25 @@ function getCategoryItems ($CatId, $lang) {
 			"discription" => "Hausgemachte Gipfeli.  Jeden Morgen frisch gebacken.",
 			"variants" =>
 			array(
-			array("variantname" => "mit Butter", "price" => 0.00),
-			array("variantname" => "Vollkorn", "price" => 0.00)
+			array("varId" => 1, "variantname" => "mit Butter", "price" => 0.00),
+			array("varId" => 2, "variantname" => "Vollkorn", "price" => 0.00)
 			)
 			),
 			array("artId" => 2, "name" => "Brötchen", "image" => "./img/user-b.png", "price" => 1.50,
 			"discription" => "Hausgemachte Brötchen. Jeden Morgen frisch gebacken.",
 			"variants" =>
 			array(
-			array("variantname" => "mit Butter", "price" => 0.00),
-			array("variantname" => "Vollkorn", "price" => 0.00),
-			array("variantname" => "Speck", "price" => 0.50)
+			array("varId" => 1, "variantname" => "mit Butter", "price" => 0.00),
+			array("varId" => 2, "variantname" => "Vollkorn", "price" => 0.00),
+			array("varId" => 3, "variantname" => "Speck", "price" => 0.50)
 			)
 			),
 			array("artId" => 3, "name" => "Brot", "image" => "./img/user-c.png", "price" => 2.00,
 			"discription" => "Hausgemachtes Brot. Jeden Morgen frisch gebacken.",
 			"variants" =>
 			array(
-			array("variantname" => "mit Butter", "price" => 0.00),
-			array("variantname" => "Vollkorn", "price" => 0.00)
+			array("varId" => 1, "variantname" => "mit Butter", "price" => 0.00),
+			array("varId" => 2, "variantname" => "Vollkorn", "price" => 0.00)
 			)
 			)
 			);
@@ -65,25 +65,25 @@ function getCategoryItems ($CatId, $lang) {
 			"discription" => "discription fr",
 			"variants" =>
 			array(
-			array("variantname" => "butter", "price" => 0.00),
-			array("variantname" => "whole-grain", "price" => 0.00)
+			array("varId" => 1, "variantname" => "butter", "price" => 0.00),
+			array("varId" => 2, "variantname" => "whole-grain", "price" => 0.00)
 			)
 			),
 			array("artId" => 2, "name" => "Bun", "image" => "./img/user-b.png", "price" => 1.50,
 			"discription" => "discription fr",
 			"variants" =>
 			array(
-			array("variantname" => "butter", "price" => 0.00),
-			array("variantname" => "whole-grain", "price" => 0.00),
-			array("variantname" => "bacon", "price" => 0.50)
+			array("varId" => 1, "variantname" => "butter", "price" => 0.00),
+			array("varId" => 2, "variantname" => "whole-grain", "price" => 0.00),
+			array("varId" => 3, "variantname" => "bacon", "price" => 0.50)
 			)
 			),
 			array("artId" => 3, "name" => "Bread", "image" => "./img/user-c.png", "price" => 2.00,
 			"discription" => "discription fr",
 			"variants" =>
 			array(
-			array("variantname" => "butter", "price" => 0.00),
-			array("variantname" => "whole-grain", "price" => 0.00)
+			array("varId" => 1, "variantname" => "butter", "price" => 0.00),
+			array("varId" => 2, "variantname" => "whole-grain", "price" => 0.00)
 			)
 			)
 			);
@@ -94,25 +94,25 @@ function getCategoryItems ($CatId, $lang) {
 			"discription" => "Homemade croissants. Freshly baked each morning.",
 			"variants" =>
 			array(
-			array("variantname" => "butter", "price" => 0.00),
-			array("variantname" => "whole-grain", "price" => 0.00)
+			array("varId" => 1, "variantname" => "butter", "price" => 0.00),
+			array("varId" => 2, "variantname" => "whole-grain", "price" => 0.00)
 			)
 			),
 			array("artId" => 2, "name" => "Bun", "image" => "./img/user-b.png", "price" => 1.50,
 			"discription" => "Homemade buns. Freshly baked each morning.",
 			"variants" =>
 			array(
-			array("variantname" => "butter", "price" => 0.00),
-			array("variantname" => "whole-grain", "price" => 0.00),
-			array("variantname" => "bacon", "price" => 0.50)
+			array("varId" => 1, "variantname" => "butter", "price" => 0.00),
+			array("varId" => 2, "variantname" => "whole-grain", "price" => 0.00),
+			array("varId" => 3, "variantname" => "bacon", "price" => 0.50)
 			)
 			),
 			array("artId" => 3, "name" => "Bread", "image" => "./img/user-c.png", "price" => 2.00,
 			"discription" => "Homemade bread. Freshly baked each morning.",
 			"variants" =>
 			array(
-			array("variantname" => "butter", "price" => 0.00),
-			array("variantname" => "whole-grain", "price" => 0.00)
+			array("varId" => 1, "variantname" => "butter", "price" => 0.00),
+			array("varId" => 2, "variantname" => "whole-grain", "price" => 0.00)
 			)
 			)
 			);
