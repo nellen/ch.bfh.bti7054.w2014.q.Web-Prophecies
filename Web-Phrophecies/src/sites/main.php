b622e1c0a66bf9fbcd917a43768ac21673b96cb6
nellen <sebastian@nellen.it>
2014-11-16 22:36:56 +0100

    added temporary welcome page

--- Web-Phrophecies/src/sites/main.php
+++ Web-Phrophecies/src/sites/main.php
@@ -1,4 +1,5 @@
 <?php 
-echo "Welcome!";
-
+echo "<h2>Welcome!</h2>";
+// TODO temporary image & styling until final image is delivered
+echo "<img src=\"./img/smiley.jpg\" style=\"width:500px;margin-left:auto;margin-right:auto;display:block;margin-bottom:20px\" />";
 ?>
