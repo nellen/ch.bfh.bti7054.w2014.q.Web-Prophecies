e22a9a8a5dae9283baf6bacaff7da7ad4963110c
burkt4 <burkt4@bfh.ch>
2014-11-16 16:15:48 +0100

    saving basket to session first draft

--- Web-Phrophecies/src/sites/article.php
+++ Web-Phrophecies/src/sites/article.php
@@ -3,12 +3,38 @@
 $lang = get_language ();
 include_once ("./includes/items.php");
 $item = getItem ( $_GET["artId"], $lang );
+
+if (isset($_POST['artId']) && isset($_POST['varId'])){
+
+	if (isset($_SESSION['basket'])){
+		//add item to basket
+		$found = false;
+		foreach ($_SESSION['basket'] as $_basketitem){
+			if($_basketitem['artId'] == $_POST['artId'] && $_basketitem['varId'] == $_POST['varId']){
+				$_basketitem['quantity'] = $_basketitem['quantity'] + 1;
+				$found = true;
+				break;
+			}
+		}
+		if (!$found){
+			$_SESSION['basket'][] = array('artId' => $_POST['artId'], 'varId'=> $_POST['varId'], 'quantity' => 1);
+		}
+	}
+	else{
+		//init basket
+		$_SESSION['basket'] = array();
+		//add item to basket
+		$_SESSION['basket'][] = array('artId' => $_POST['artId'], 'varId'=> $_POST['varId'], 'quantity' => 1);
+	}
+}
+
+
 echo "<div class=\"article\">";
 echo "<h1>". $item["name"] . "</h1>";
 echo "<img src=\"./img/user-a.png\" width=\"100px\" height=\"100px\"/>";
 echo "<p class=\"article-price\">".$priceLabel.": ". $item["price"] . " CHF</p>";
 
-echo "<form action=\"index.php?site=billing&lang=".$lang."\" method=\"post\">";
+echo "<form action=\"index.php?". $_SERVER['QUERY_STRING']."\" method=\"post\">";
 echo "<input type=\"hidden\" name=\"artId\" value=\"". $item ["artId"]. "\" /input>";
 echo "<select name =\"varId\" size=\"1\">";
 echo "<option value=\"0\">normal</option>";
--- Web-Phrophecies/src/sites/basket.php
+++ Web-Phrophecies/src/sites/basket.php
@@ -1,4 +1,53 @@
 <?php
-	echo "Basket";
-	echo "<img src=\"./img/under_construction.png\" width=\"100%\" height=\"100%\" />";
+
+include_once ("./includes/items.php");
+$lang = get_language ();
+
+if (!isset($_SESSION['basket'])){
+	echo "Shopping basket is empty";
+}
+else{
+	echo "<table border=\"1px\">";
+	echo "<thead>";
+	echo "<tr>";
+	echo "<th>Quantity</th>";
+	echo "<th>Description</th>";
+	echo "<th>Variation</th>";
+	echo "<th>Price per Unit</th>";
+	echo "<th>Price</th>";
+	echo "<th>&nbsp;</th>";
+	echo "</tr>";
+	echo "</thead>";
+	echo "<tbody>";
+	
+	foreach ($_SESSION['basket'] as $_basketitem){
+		$item = getItem ( $_basketitem["artId"], $lang );
+		$pricePerUnit = $item['price'];
+		echo "<tr>";
+		echo 	"<td>";
+		echo		"<input type=\"text\" maxlength=\"4\" size=\"5\" readonly=\"readonly\" value=\"".$_basketitem['quantity']. "\" />";
+		echo 	"</td>";
+		echo	"<td>".$item['name']."</td>";
+		if ($_basketitem["varId"] == 0){
+			echo	"<td>normal</td>";
+		}
+		else{
+			foreach ( $item ["variants"] as $variant ) {
+				if($variant ["varId"] == $_basketitem["varId"]){
+					echo	"<td>".$variant ["variantname"]."</td>";
+					$pricePerUnit = $pricePerUnit + $variant ["price"];
+					break;
+				}
+			}
+		}
+		echo	"<td>".$pricePerUnit."</td>";
+		echo	"<td>". $pricePerUnit * $_basketitem['quantity']."</td>";
+		echo	"<td> <img src=\"./img/trash.png\" width=\"30px \" onclick=\"msgDeleted()\" /></td>";
+		echo "</tr>";
+	}
+	echo "</tbody>";
+	echo "</table>";
+}
+
+
 ?>
