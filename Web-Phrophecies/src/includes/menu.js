965ef525cd2305ff32b6e7a9b174cf1b68d765a6
surferboy87 <surferboy87@github.com>
2014-11-17 17:55:05 +0100

    included javascript for playing with submenus. Has to change location to a js-folder

---
+++ Web-Phrophecies/src/includes/menu.js
@@ -0,0 +1,7 @@
+//function openSubMenu(){
+//	document.getElementById("dropdown").style.display = "block";
+//}
+
+//function closeSubMenu(){
+//	document.getElementById("dropdown").style.display = "none";
+//}
