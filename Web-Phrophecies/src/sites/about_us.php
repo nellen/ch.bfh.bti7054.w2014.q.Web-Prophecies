6baf388cbe16ff7595c82e1a303b4982293b821e
nellen <sebastian@nellen.it>
2014-12-31 00:07:03 +0100

    added html templates to contact and about us page. the contact form is still a place holder

--- Web-Phrophecies/src/sites/about_us.php
+++ Web-Phrophecies/src/sites/about_us.php
@@ -1,69 +1,59 @@
-<?php
+<?php 
+
 require_once './includes/skypestatus.inc.php';
 
 $status_burkt4 = getSkypeStatus("burkt4", $lang, "smallicon");
 $status_huber3 = getSkypeStatus("surferboy8722", $lang, "smallicon");
 $status_nells1 = getSkypeStatus("sebastian.nellen", $lang, "smallicon");
 
-?>
-<h1>
-<?php
+// fill $title, $description, $subTitle
 switch ($lang) {
 	case "en" :
-		echo 'About us';
-		break;
-	case "de" :
-		echo '&Uuml;ber uns';
-		break;
-	case "fr" :
-		echo "A propos de BreakFast";
-		break;
-}
-?>
-</h1>
-<p>
-<?php
-switch ($lang) {
-	case "en" :
-		echo 'The <b>BreakFast Company</b> is a start-up, which was founded in early 2014.
+		$title = 'About us';
+		$description = 
+			'The <b>BreakFast Company</b> is a start-up, which was founded in early 2014.
 			Our goal is to provide you a good quality breakfast in less time and
-			with less effort.<br /> 
+			with less effort.<br />
 			Our young and dynamic team is waiting to serve
 			you a rich and/or fast breakfast to go - either booked in advance or by
 			ordering in our shop.<br />';
+		$subTitle = 'Our team';
 		break;
 	case "de" :
-		echo 'Die <b>BreakFast Company</b> ist ein Start-Up, welches Anfang Jahr 2014 
-			gegr&uuml;ndet wurde. Unser Ziel ist es, qualitativ hochwertiges Fr&uuml;hst&uuml;ck in 
+		$title = '&Uuml;ber uns';
+		$description =
+			'Die <b>BreakFast Company</b> ist ein Start-Up, welches Anfang Jahr 2014
+			gegr&uuml;ndet wurde. Unser Ziel ist es, qualitativ hochwertiges Fr&uuml;hst&uuml;ck in
 			k&uuml;rzerer Zeit mit weniger Aufwand zu liefern.<br />
-			Unser junges und dynamisches Team freut sich darauf Ihnen ein reiches 
+			Unser junges und dynamisches Team freut sich darauf Ihnen ein reiches
 			und/oder schnelles Fr&uuml;hst&uuml;ck "to go" zu servieren -
 			egal ob Sie es im Voraus gebucht haben oder direkt in unserem Laden bestellen.<br />';
+		$subTitle = 'Unser Team';
 		break;
 	case "fr" :
-		echo 'L\'entreprise <b>BreakFast Company</b> est un "start-up", qu\'était fondue en 2014.
+		$title = "A propos de BreakFast";
+		$description = 
+			'L\'entreprise <b>BreakFast Company</b> est un "start-up", qu\'était fondue en 2014.
 			Notre but sera de vous propose un déjeuner d\'une qualité excellente dans un temps minimal.<br />
-			Nos déjeuners sont préparés par notre team à la minute - 
+			Nos déjeuners sont préparés par notre team à la minute -
 			n\'importe si l\'ordre est placée en ligne ou en magasin.<br />';
+		$subTitle = "Notre team";
 		break;
 }
-?>
-</p>
-<h2>
-<?php
-switch ($lang) {
-	case "en" :
-		echo 'Our team';
-		break;
-	case "de" :
-		echo 'Unser Team';
-		break;
-	case "fr" :
-		echo "Notre team";
-		break;
+
+function fill_template(&$template, $tag, $content) {
+	$template = str_replace("@$tag@", $content, $template);
 }
+
+$template = file_get_contents(ROOT."/sites/about_us.html");
+fill_template($template, "title", $title);
+fill_template($template, "description", $description);
+fill_template($template, "subTitle", $subTitle);
+
+echo $template;
+
 ?>
-</h2>
+
 <table>
 	<tr>
 		<td><img src="./img/user-a.png" width="100px" /></td>
@@ -93,4 +83,3 @@ switch ($lang) {
 		</td>
 	</tr>
 </table>
-<p></p>
--- Web-Phrophecies/src/sites/contact.php
+++ Web-Phrophecies/src/sites/contact.php
@@ -1,149 +1,73 @@
-<h1>
-<?php
-switch ($lang) {
-	case "en" :
-		echo 'Contact';
-		break;
-	case "de" :
-		echo 'Kontakt';
-		break;
-	case "fr" :
-		echo "Contact";
-		break;
-}
-?>
-</h1>
-<?php
+<?php 
 
+function fill_template(&$template, $tag, $content) {
+	$template = str_replace("@$tag@", $content, $template);
+}
+
+// fill $title, $address, $contactNumbers, $contactUs
 switch ($lang) {
 	case "en" :
-		echo '<p>
-				The BreakFast Company<br />
+		$title = 'Contact';
+		$address = 
+				'The BreakFast Company<br />
 				Aarefeldstrasse 16<br />
 				3600 Thun
-				<br /> <br />
-			</p>
-			<table id="contact-numbers">
-				<tr>
+				<br /> <br />';
+		$contactNumbers = 
+				'<tr>
 					<td>Phone:&nbsp;&nbsp;</td>
 					<td>+1 123 456 78</td>
 				</tr>
 				<tr>
 					<td>Fax:</td>
 					<td>+1 123 456 79</td>
-				</tr>
-			</table>
-			<p>
-			<iframe width="500" height="350" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?q=Aarefeldstrasse%2016%2C%20Thun%2C%20Switzerland&key=AIzaSyAYsl0o--f9WQIwjud-6GxCGAfTBwVRV_0"></iframe>
-			<br />
-			</p>
-			<hr />
-			<p>
-				<br />
-				Feel free to leave us a message with the contact form below.
-				<br />
-			</p>';
+				</tr>';
+		$contactUs = 'Feel free to leave us a message with the contact form below.';
 		break;
 	case "de" :
-		echo '<p>
-				The BreakFast Company<br />
+		$title = 'Kontakt';
+		$address =
+				'The BreakFast Company<br />
 				Aarefeldstrasse 16<br />
 				3600 Thun
-				<br /> <br />
-			</p>
-			<table id="contact-numbers">
-				<tr>
+				<br /> <br />';
+		$contactNumbers =
+				'<tr>
 					<td>Telefon:&nbsp;&nbsp;</td>
 					<td>+1 123 456 78</td>
 				</tr>
 				<tr>
 					<td>Fax:</td>
 					<td>+1 123 456 79</td>
-				</tr>
-			</table>
-			<p>
-				<iframe width="500" height="350" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?q=Aarefeldstrasse%2016%2C%20Thun%2C%20Switzerland&key=AIzaSyAYsl0o--f9WQIwjud-6GxCGAfTBwVRV_0"></iframe>
-				<br />
-			</p>
-			<hr />
-			<p>
-				<br />
-				Kontaktieren Sie uns mittels unten stehendem Formular. Wir freuen uns &uuml;ber Ihre Nachricht.
-				<br />
-			</p>';
+				</tr>';
+		$contactUs = 'Kontaktieren Sie uns mittels unten stehendem Formular. Wir freuen uns &uuml;ber Ihre Nachricht.';
 		break;
 	case "fr" :
-		echo '<p>
-				The BreakFast Company<br />
+		$title = "Contact";
+		$address =
+				'The BreakFast Company<br />
 				Aarefeldstrasse 16<br />
 				3600 Thoune
-				<br /> <br />
-			</p>
-			<table id="contact-numbers">
-				<tr>
+				<br /> <br />';
+		$contactNumbers =
+				'<tr>
 					<td>Téléphone:&nbsp;&nbsp;</td>
 					<td>+1 123 456 78</td>
 				</tr>
 				<tr>
 					<td>Fax:</td>
 					<td>+1 123 456 79</td>
-				</tr>
-			</table>
-			<p>
-			<iframe width="500" height="350" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?q=Aarefeldstrasse%2016%2C%20Thun%2C%20Switzerland&key=AIzaSyAYsl0o--f9WQIwjud-6GxCGAfTBwVRV_0"></iframe>
-			<br />
-			</p>
-			<hr />
-			<p>
-				<br />
-				N\'hésitez pas à nous contacter avec le formulaire ci-dessous.
-				<br />
-			</p>';
+				</tr>';
+		$contactUs = 'N\'hésitez pas à nous contacter avec le formulaire ci-dessous.';
 		break;
 }
+
+$template = file_get_contents(ROOT."/sites/contact.html");
+fill_template($template, "title", $title);
+fill_template($template, "address", $address);
+fill_template($template, "contactNumbers", $contactNumbers);
+fill_template($template, "contactUs", $contactUs);
+
+echo $template;
+
 ?>
-
-
-<form name="htmlform" method="post"
-	action="./includes/html_form_send.php">
-	<table width="550px">
-		<tr>
-			<td valign="top"><label for="first_name">First Name *</label></td>
-			<td valign="top"><input type="text" name="first_name" maxlength="50"
-				size="30" /></td>
-		</tr>
-
-		<tr>
-			<td valign="top"><label for="last_name">Last Name *</label></td>
-			<td valign="top"><input type="text" name="last_name" maxlength="50"
-				size="30" /></td>
-		</tr>
-		<tr>
-			<td valign="top"><label for="email">Email Address *</label></td>
-			<td valign="top"><input type="text" name="email" maxlength="80"
-				size="30" /></td>
-
-		</tr>
-		<tr>
-			<td valign="top"><label for="telephone">Telephone Number</label></td>
-			<td valign="top"><input type="text" name="telephone" maxlength="30"
-				size="30" /></td>
-		</tr>
-		<tr>
-			<td valign="top"><label for="comments">Comments *</label></td>
-			<td valign="top"><textarea name="comments" cols="50" rows="6"></textarea></td>
-
-		</tr>
-		<tr>
-			<td colspan="2" style="text-align: center"><input type="submit"
-				value="Submit" /> ( <a
-				href="http://www.freecontactform.com/html_form.php">based on HTML
-					Form</a>)</td>
-		</tr>
-		<tr>
-			<td>
-				<p>&nbsp;</p>
-			</td>
-		</tr>
-	</table>
-</form>
