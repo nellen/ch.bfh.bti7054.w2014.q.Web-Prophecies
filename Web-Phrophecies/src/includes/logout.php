505bb42cf528c944da65fd435186f9ffb43b5011
burkt4 <burkt4@bfh.ch>
2015-01-02 10:37:50 +0100

    logout does not delete basket

--- Web-Phrophecies/src/includes/logout.php
+++ Web-Phrophecies/src/includes/logout.php
@@ -1,6 +1,5 @@
 <?php 
 session_start();   // create or recover session,
-session_unset();   // ... delete all session variables,
-session_destroy(); // ... and end it
+unset($_SESSION ["user"]); // delete only the user session variable to preserve basket
 header ("Location: ../index.php"); // go back to index - but logged out
 ?>
